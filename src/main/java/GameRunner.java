51112a33a251b15b0d588067d0339e565453e862
Quatrani Paul <quatranipaul@zipcodes-MacBook-Pro-9.local>
2021-12-27 12:24:34 -0500

    created todo list

--- src/main/java/GameRunner.java
+++ src/main/java/GameRunner.java
@@ -1,3 +1,12 @@
+//TODO
+//puzzles
+//random battles
+//items in general
+//stat changing items
+//make playable
+//serializability
+//story
+
 public class GameRunner {
     public static void main(String[] args) {
         //take command line args?
